27e2e5d923a137843ec48612a7141eadab79531d
sheng.jiang <sheng.jiang@accentute.com>
2017-09-20 14:40:46 +0800

    add home work Day2

--- app/src/main/java/com/accenture/accenturetairningdemo/MainActivity.java
+++ app/src/main/java/com/accenture/accenturetairningdemo/MainActivity.java
@@ -1,34 +1,112 @@
 package com.accenture.accenturetairningdemo;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.TextView;
 
+import com.accenture.accenturetairningdemo.fragment.ContactFragment;
+import com.accenture.accenturetairningdemo.fragment.ManageFragment;
+import com.accenture.accenturetairningdemo.fragment.MeFragment;
+import com.ashokvarma.bottomnavigation.BottomNavigationBar;
+import com.ashokvarma.bottomnavigation.BottomNavigationItem;
 
-public class MainActivity extends AppCompatActivity {
-    TextView toFirstPage;
-    TextView textView;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationBar.OnTabSelectedListener{
+
+    private ArrayList<Fragment> fragments;
+    private boolean isFirstClick = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        textView = (TextView) this.findViewById(R.id.textView);
-        toFirstPage = (TextView) this.findViewById(R.id.toFirstPage);
-//        toFirstPage.setText("hello world");
-//        toFirstPage.setText("导航页面");
-//        EventBus.getDefault().register(this);
-        toFirstPage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-//                Log.e("test log","点击文本");
-                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                intent.putExtra("param", "123");
-                startActivity(intent);
 
-            }
-        });
+        BottomNavigationBar bottomNavigationBar = (BottomNavigationBar) findViewById(R.id.bottom_navigation_bar);
+        bottomNavigationBar.setMode(BottomNavigationBar.MODE_FIXED);
+        bottomNavigationBar
+                .setBackgroundStyle(BottomNavigationBar.BACKGROUND_STYLE_STATIC
+                );
+        bottomNavigationBar.addItem(new BottomNavigationItem(R.drawable.bluemap_hover, "蓝图").setActiveColorResource(R.color.colorPrimary))
+                .addItem(new BottomNavigationItem(R.drawable.monitor_hover, "监控").setActiveColorResource(R.color.colorPrimary))
+                .addItem(new BottomNavigationItem(R.drawable.me_hover, " 我的").setActiveColorResource(R.color.colorPrimary))
+                .setFirstSelectedPosition(0)
+                .initialise();
+//
+        fragments = getFragments();
+        setDefaultFragment();
+        bottomNavigationBar.setTabSelectedListener(this);
+
     }
+
+    /**
+     * 设置默认的
+     */
+    private void setDefaultFragment() {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+        transaction.replace(R.id.layFrame, ManageFragment.newInstance("",""));
+        transaction.commit();
+    }
+
+    private ArrayList<Fragment> getFragments() {
+        ArrayList<Fragment> fragments = new ArrayList<>();
+        fragments.add(ManageFragment.newInstance("",""));
+        fragments.add(ContactFragment.newInstance("",""));
+        fragments.add(MeFragment.newInstance("",""));
+//        fragments.add(TvFragment.newInstance("Movies & TV"));
+//        fragments.add(GameFragment.newInstance("Games"));
+        return fragments;
+    }
+
+
+
+
+
+    @Override
+    public void onTabSelected(int position) {
+        if (fragments != null) {
+//            if(isFirstClick){
+//                FragmentManager fm = getSupportFragmentManager();
+//                FragmentTransaction ft = fm.beginTransaction();
+//                Fragment fragment = fragments.get(0);
+//                ft.remove(fragment);
+//                ft.commitAllowingStateLoss();
+//                isFirstClick = false;
+//            }
+            if (position < fragments.size()) {
+                FragmentManager fm = getSupportFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
+                Fragment fragment = fragments.get(position);
+//                if (fragment.isAdded()) {
+//                    ft.replace(R.id.layFrame, fragment);
+//                } else {
+//                    ft.add(R.id.layFrame, fragment);
+//                }
+                ft.replace(R.id.layFrame, fragment);
+                ft.commitAllowingStateLoss();
+            }
+        }
+
+    }
+
+    @Override
+    public void onTabUnselected(int position) {
+        if (fragments != null) {
+            if (position < fragments.size()) {
+                FragmentManager fm = getSupportFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
+                Fragment fragment = fragments.get(position);
+                ft.remove(fragment);
+                ft.commitAllowingStateLoss();
+            }
+        }
+    }
+
+    @Override
+    public void onTabReselected(int position) {
+
+    }
+
 }
