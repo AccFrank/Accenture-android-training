a7e91b96aba80bf66f0515ac0229eafa8e0c524d
sheng.jiang <sheng.jiang@accentute.com>
2017-09-20 14:14:40 +0800

    add.project

--- app/src/main/java/com/accenture/accenturetairningdemo/MainActivity.java
+++ app/src/main/java/com/accenture/accenturetairningdemo/MainActivity.java
@@ -1,34 +1,46 @@
 package com.accenture.accenturetairningdemo;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.TextView;
+
+import butterknife.ButterKnife;
 
 
 public class MainActivity extends AppCompatActivity {
-    TextView toFirstPage;
-    TextView textView;
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        textView = (TextView) this.findViewById(R.id.textView);
-        toFirstPage = (TextView) this.findViewById(R.id.toFirstPage);
-//        toFirstPage.setText("hello world");
-//        toFirstPage.setText("导航页面");
-//        EventBus.getDefault().register(this);
-        toFirstPage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-//                Log.e("test log","点击文本");
-                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                intent.putExtra("param", "123");
-                startActivity(intent);
+        ButterKnife.bind(this);
 
-            }
-        });
     }
+
+
 }
