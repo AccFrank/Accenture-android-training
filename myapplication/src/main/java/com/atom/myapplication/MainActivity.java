b0b0a9afe044dfffd8967ddee9613e9af0440963
PotStove <935641689@qq.com>
2017-09-25 23:11:02 +0800

    add bottom bar

--- myapplication/src/main/java/com/atom/myapplication/MainActivity.java
+++ myapplication/src/main/java/com/atom/myapplication/MainActivity.java
@@ -2,6 +2,8 @@ package com.atom.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.ashokvarma.bottomnavigation.BottomNavigationBar;
+import com.ashokvarma.bottomnavigation.BottomNavigationItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationBar bottomNavigationBar = (BottomNavigationBar) findViewById(R.id.bottom_navigation_bar);
+        bottomNavigationBar.addItem(new BottomNavigationItem(R.drawable.ic_launcher, "List"));
+        bottomNavigationBar.addItem(new BottomNavigationItem(R.drawable.ic_launcher, "Blank"));
+        bottomNavigationBar.addItem(new BottomNavigationItem(R.drawable.ic_launcher, "Setting"));
+        bottomNavigationBar.initialise();
+
+        bottomNavigationBar.setTabSelectedListener(new BottomNavigationBar.OnTabSelectedListener(){
+            @Override
+            public void onTabSelected(int position) {
+            }
+            @Override
+            public void onTabUnselected(int position) {
+            }
+            @Override
+            public void onTabReselected(int position) {
+            }
+        });
     }
 }
